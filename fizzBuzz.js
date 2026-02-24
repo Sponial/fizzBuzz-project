3e8e77e32771ddbcf51cbfb05d9bb82e66232d6e
Orson Stawiarski <orson@stazca.com>
2026-02-24 15:56:38 +0700

    changed The output.push count and count++ from standard syntax to an else conditional.

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,8 +3,6 @@ var count = 1;
 
 function fizzBuzz() {
 //write code here
-output.push(count);
-count++;
     if (count % 5 === 0 && count % 3 === 0) { 
     output.push("FizzBuzz")
     count++; }
@@ -16,7 +14,14 @@ count++;
     output.push("Buzz")
     count++;
     }
+    else {output.push(count);
+    count++;}
     
 console.log(output);
 }
 fizzBuzz();
+//Okay, so in the case of printing culling fizzbuzz, we had at first one, and then on the second time we had two and fizz. 
+//So that would mean the code we have, output.push count at the top runs all the time no matter what. Count will always be pushed, and that is apparently separate to output.push, and count++ is outside of the conditional. So output.push, count, and count++ are outside of the conditionals. 
+//So no matter what happens every single time, will be pushed and after that's done it'll be added plus one. Okay, so we know that that's always running. That's the solution for our multiple coles being placed. There's no differentiator saying that one doesn't get pasted or printed if the other one is printed as well. 
+
+//
