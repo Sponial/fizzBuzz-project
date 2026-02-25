174ff78475d77ec3c07a16219f4a6be6728800bb
Orson Stawiarski <orson@stazca.com>
2026-02-25 11:23:05 +0700

    Adding while loop to the FizzBuzz function.

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -3,6 +3,7 @@ var count = 1;
 
 function fizzBuzz() {
 //write code here
+    while (count <= 100) {
     if (count % 5 === 0 && count % 3 === 0) { 
     output.push("FizzBuzz")
     count++; }
@@ -16,7 +17,8 @@ function fizzBuzz() {
     }
     else {output.push(count);
     count++;}
-    
+    }
+
 console.log(output);
 }
 fizzBuzz();
@@ -24,4 +26,4 @@ fizzBuzz();
 //So that would mean the code we have, output.push count at the top runs all the time no matter what. Count will always be pushed, and that is apparently separate to output.push, and count++ is outside of the conditional. So output.push, count, and count++ are outside of the conditionals. 
 //So no matter what happens every single time, will be pushed and after that's done it'll be added plus one. Okay, so we know that that's always running. That's the solution for our multiple coles being placed. There's no differentiator saying that one doesn't get pasted or printed if the other one is printed as well. 
 
-//
+//Success! Pasting the output.push(count) and the count++ into an else successfully produced the result that we were looking for. 
